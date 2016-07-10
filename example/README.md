8f7a3cc1de8983ca40fc0ca48160607b91b71fb4
suppi <soupiral@gmail.com>
2016-07-11 00:04:40 +0300

    updating for ps 0.9.1

---
+++ example/README.md
@@ -0,0 +1,8 @@
+Example
+-------
+
+To build, use:
+
+```sh
+pulp browserify --to app.js
+```
--- src/Audio/Howler.purs
+++ src/Audio/Howler.purs
@@ -4,7 +4,7 @@ module Audio.Howler
     ( HOWLER
     , Howl
     , Props
-    , Fade
+--    , Fade
     , new
     , play
     , stop
